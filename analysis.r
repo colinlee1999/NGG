344960917aa79511ca040c2edccbee5b2e499952
colinlee1999 <arteest@foxmail.com>
2016-01-13 14:23:21 -0500

    test file modified

---
+++ analysis.r
@@ -0,0 +1,2 @@
+print(table(fData(result$E_Set)$est_cluster,fData(result$E_Set)$true_cluster))
+print(result)
--- test.r
+++ test.r
@@ -1,15 +1,20 @@
 source('NGG_lp.r')
 source('gen_data_by_NGG_lp.r')
 
+start_time = proc.time()
+
 G = 10000
-n = 20
+n = 30
 
 psi = c(
-	2,1,2,1,
-	2,1,2,1,
-	2,1)
+	1,0.5,-1,3,
+	1,0.5,-1,3,
+	1,0.5)
 
 t_pi = c(0.05, 0.05, 0.90)
 
 E_Set = gen_data_by_NGG_lp(G, n, psi, t_pi)
 result = NGG_lp(E_Set, verbose = 1, is_sim = 1, M = 100, limma_prior = 0)
+
+end_time = proc.time()
+print(end_time - start_time)
