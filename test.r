3fddc5a9e0fa58d69ba1f7e3fc24e3f1b0014814
colinlee1999 <arteest@foxmail.com>
2016-01-15 17:47:29 -0500

    minor update

--- test.r
+++ test.r
@@ -3,23 +3,23 @@ source('gen_data_by_NGG_lp.r')
 
 start_time = proc.time()
 
-G = 10000
+G = 1000
 n = 30
 
+# psi = c(
+# 	2,1,2,1,
+# 	2,1,2,1,
+# 	2,1)
+
+# psi = c(
+# 	1,0.5,1,0.5,
+# 	1,0.5,1,0.5,
+# 	1,0.5)
+
 psi = c(
-	2,1,2,1,
-	2,1,2,1,
-	2,1)
-
-# psi = c(
-# 	1,0.5,1,0.5,
-# 	1,0.5,1,0.5,
-# 	1,0.5)
-
-# psi = c(
-# 	1,0.5,-1,3,
-# 	1,0.5,-1,3,
-# 	1,0.5)
+	1,0.5,-1,3,
+	1,0.5,-1,3,
+	1,0.5)
 
 # psi = c(
 # 	0,-2,1,0.5,
@@ -28,7 +28,7 @@ psi = c(
 
 t_pi = c(0.05, 0.05, 0.90)
 
-generate = 0
+generate = 1
 
 if (generate)
 {
@@ -38,8 +38,12 @@ if (generate)
 {
 	E_Set = readRDS('E_Set.Rdata')
 }
-result = NGG_lp(E_Set, verbose = 1, is_sim = 1, M = 100, limma_prior = 0, cores = 2)
+result = NGG_lp(E_Set, verbose = 1, is_sim = 1, M = 100, limma_prior = 0, cores = 2, converge_threshold = 1e-4)
 
 end_time = proc.time()
-result$runnint_time = end_time - start_time
+result$running_time = end_time - start_time
+result$t_pi = t_pi
+result$psi = psi
+result$G = G
+result$n = n
 print(end_time - start_time)
